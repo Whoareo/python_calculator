9e1562d37bb10b01b65659955073ac711ff9da83
whoareo <peapope12562679@gmail.com>
2024-05-27 18:57:18 +0700

    Updated calculator.py

--- calculator.py
+++ calculator.py
@@ -1,37 +1,41 @@
-def tong(phan_tu) -> int | float:
+def total(elements: list[int]) -> int | float:
     result: int = 0
-    for element in phan_tu:
+    for element in elements:
         result += element
     return result
 
 
-def tich(phan_tu) -> int | float:
+def multiple(elements: list[int]) -> int | float:
     result: int = 1
-    for element in phan_tu:
+    for element in elements:
         result *= element
     return result
 
 
-user_input: str = input("Nhập phép tính: ")
-numbers: list[str]
-if "+" in user_input:
-    numbers: list[str] = user_input.split("+")
-elif "x" in user_input:
-    numbers: list[str] = user_input.split("x")
-elif "*" in user_input:
-    numbers: list[str] = user_input.split("*")
-else:
-    print("Loi")
-    exit(0)
-
-for index, number in enumerate(numbers):
-    if number.isnumeric():
-        numbers[index]: int = int(number)
+def split_string(string: str) -> list[str] | None:
+    if "+" in string:
+        return string.split("+")
     else:
-        print("Loi")
+        print("Vui lòng nhập xâu hợp lệ.")
         exit(0)
-else:
-    if "+" in user_input:
-        print(tong(numbers))
+
+
+def process(array: list[str]) -> int | None:
+    for index, number in enumerate(array):
+        if number.isnumeric():
+            array[index]: int = int(number)
+        elif "x" in number:
+            array[index]: int = multiple([int(element) for element in number.split("x")])
+        elif "*" in number:
+            array[index]: int = multiple(([int(element) for element in number.split("*")]))
+        else:
+            print("Lỗi.")
+            exit(0)
     else:
-        print(tich(numbers))
+        return total(array)
+
+
+user_input: str = input("Nhập phép tính: ")
+numbers: list[str] = split_string(user_input)
+
+print("Kết quả: ", process(numbers))
